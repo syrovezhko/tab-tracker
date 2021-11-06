bed890688e33d50ad20a501f5bedbf4ed1dccea5
syrovezhko nikolai <syrovezhk@list.ru>
2021-11-06 21:25:14 +0300

    connected sqlite db, made an authentication controller and policies

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,23 +1,33 @@
 <template>
-  <div>
-    <h1>Register</h1>
-    <input 
-      type="email" 
-      name="email"
-      v-model="email"
-      placeholder="email"/>
-      <br>
-    <input 
-      type="password" 
-      name="password"
-      v-model="password"
-      placeholder="password"/>
-    <br>
-    <button
-      @click="register">
-      Register
-    </button>
-  </div>
+  <v-layout column>
+    <v-flex xs6 offset-xs3>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="cyan" dark>
+          <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
+        <div class="checkin">
+          <input
+            type="email"
+            name="email"
+            v-model="email"
+            placeholder="email"/>
+            <br>
+          <input
+            type="password"
+            name="password"
+            v-model="password"
+            placeholder="password"/>
+          <br>
+          <div class="error" v-html="error" />
+          <v-btn
+            class="cyan"
+            @click="register">
+            Register
+          </v-btn>
+        </div>
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
@@ -26,20 +36,31 @@ export default {
   data () {
     return {
       email: '',
-      password: ''
+      password: '',
+      error: null
     }
   },
   methods: {
     async register () {
-      const response = await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
-      console.log(response.data)
+      try {
+        const response = await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+        console.log(response.data)
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }
   }
 }
 </script>
 
 <style scoped>
+.error {
+  color: red;
+}
+.checkin {
+  padding: 2px 4px 2px 4px
+}
 </style>
