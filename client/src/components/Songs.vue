f36db3345cdfc80c9c67c6f31de2782ef7b45c5f
syrovezhko nikolai <syrovezhk@list.ru>
2021-12-06 11:13:58 +0300

    added style for songs page

--- client/src/components/Songs.vue
+++ client/src/components/Songs.vue
@@ -1,12 +1,22 @@
 <template>
   <div class="row justify-content-center">
     <panel title="Songs">
-      <div class="row justify-content-between border border-2 border-secondary border-top-0 border-start-0 border-end-0"
+      <div class="row justify-content-start align-items-center border border-2 border-secondary border-top-0 border-start-0 border-end-0"
         v-for="song in songs"
         :key="song.id">
-          <div class="col-4 col-xs-12 col-md-3">{{song.title}}</div>
-          <div class="col-3 col-xs-12 col-md-3">{{song.artist}}</div>
-          <div class="col-5 col-xs-12 col-md-3">{{song.albom}}</div>
+          <div class="col-1">{{song.id}}</div>
+          <div class="col-3 col-md-1">
+            <img :src="song.albomImageUrl" alt="" class="">
+          </div>
+          <div class="col-7 col-md-4">
+            <div class="row justify-content-start align-items-center py-2">
+              <div class="col-12 text-start">{{song.title}}</div>
+              <div class="col-12 text-start">{{song.artist}}</div>
+            </div>
+          </div>
+          <div class="col-5 d-none d-md-flex">
+            {{song.albom}}
+          </div>
       </div>
     </panel>
   </div>
@@ -32,5 +42,10 @@ export default {
 </script>
 
 <style scoped>
-
+  img {
+    height: 50px;
+    border-radius: 10px;
+    margin-top: 10px;
+    margin-bottom: 10px;
+  }
 </style>
